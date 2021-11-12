fca62874ddbf44d08a4e497057b9afaf1257a192
tjysdsg <tjy1018543509@163.com>
2021-11-12 19:58:45 +0800

    Save more detailed data in count_phone_code_map.py

--- count_phone_code_map.py
+++ count_phone_code_map.py
@@ -16,6 +16,33 @@ def get_args():
     return parser.parse_args()
 
 
+def get_top_n(mapping: dict):
+    ret = {}
+    for k, seq in mapping.items():
+        vals, counts = np.unique(seq, return_counts=True)
+        vals = vals.tolist()
+        argidx = np.argsort(counts)[::-1]
+        n = len(vals)
+        ret[k] = {vals[argidx[i]]: int(counts[argidx[i]]) for i in range(n)}
+    return ret
+
+
+def top_n_to_percent(mapping: dict):
+    ret = {}
+    for k, counts in mapping.items():
+        total = 0
+        for _, n in counts.items():
+            total += n
+
+        percent = {}
+        for p, n in counts.items():
+            percent[p] = n / total
+
+        ret[k] = percent
+
+    return ret
+
+
 def main():
     args = get_args()
     out_dir = args.out_dir
@@ -23,6 +50,7 @@ def main():
     os.makedirs(out_dir, exist_ok=True)
 
     code2phones = {}
+    phone2codes = {}
     with open(args.phone_alignment) as f:
         for line in f:
             utt, start, end, phone = line.strip('\n').split()
@@ -37,18 +65,22 @@ def main():
                 print(e)
                 continue
 
-            for ci in code_idx:
-                code2phones.setdefault(int(ci), []).append(phone)
+            for ci in code_idx.tolist():
+                code2phones.setdefault(ci, []).append(phone)
+                phone2codes.setdefault(phone, []).append(ci)
 
     # get top phone counts of each code
-    code2counts = {}
-    for ci, phones in code2phones.items():
-        vals, counts = np.unique(phones, return_counts=True)
-        argidx = np.argsort(counts)[::-1]
-        n = len(vals)
-        code2counts[ci] = {vals[argidx[i]]: int(counts[argidx[i]]) for i in range(n)}
+    code2counts = get_top_n(code2phones)
+
+    # get top code counts of each phone
+    phone2counts = get_top_n(phone2codes)
 
     json.dump(code2counts, open(os.path.join(out_dir, 'code2counts.json'), 'w'), indent='  ')
+    json.dump(phone2counts, open(os.path.join(out_dir, 'phone2counts.json'), 'w'), indent='  ')
+
+    # get percentage
+    json.dump(top_n_to_percent(code2counts), open(os.path.join(out_dir, 'code2percent.json'), 'w'), indent='  ')
+    json.dump(top_n_to_percent(phone2counts), open(os.path.join(out_dir, 'phone2percent.json'), 'w'), indent='  ')
 
 
 if __name__ == '__main__':
