1158af33f995b1e279056c57440944eedf6cee11
唐际洋 <tangjiyang@xiaomi.com>
2021-12-10 16:36:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 # generate PaT, PaC, TaP triplets
-python generate_item_file.py --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode --out-dir=exp
+python generate_item_file.py \
+  --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_l2arctic \
+  --phone-alignment=phone_alignment.txt \
+  --out-dir=exp
 
 # calculate ABX score using the triplets and codes
 python calc_abx.py
 
 # count the mapping between each code and phone
-python count_phone_code_map.py --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_i --out-dir=exp
+python count_phone_code_map.py \
+  --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_l2arctic_i \
+  --phone-alignment=phone_alignment.txt \
+  --out-dir=exp
 
 # visualize the phone-code mapping as parallel sets diagram
 # python visualize_code2counts.py
+
