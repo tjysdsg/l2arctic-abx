0b20a00d714b27b48b3a38efef9a16bd48652354
唐际洋 <tangjiyang@xiaomi.com>
2021-12-13 17:54:16 +0800

    Add shortcut

--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
 # generate PaT, PaC, TaP triplets
-# python generate_item_file.py \
-#   --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_l2arctic \
-#   --phone-alignment=phone_alignment.txt \
-#   --out-dir=exp
-
-# or test on librispeech test clean:
-#   --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_LS-test-clean \
-#   --utt2spk=/home/storage15/tangjiyang/librispeech_ali_test_clean/utt2spk \
-#   --phone-alignment=phone_alignment.txt \
-#   --out-dir=exp_LS_LS-test-clean
+python generate_item_file.py \
+  --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_l2arctic \
+  --phone-alignment=phone_alignment.txt \
+  --out-dir=exp
 
 # calculate ABX score using the triplets and codes
 python calc_abx.py --data-dir=exp
---
+++ test_LS-model_LS-test-clean.sh
@@ -0,0 +1,8 @@
+python generate_item_file.py \
+  --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_librispeech960_LS-test-clean \
+  --utt2spk=/home/storage15/tangjiyang/librispeech_ali_test_clean/kaldi_format/utt2spk \
+  --phone-alignment=/home/storage15/tangjiyang/librispeech_ali_test_clean/align.txt \
+  --out-dir=exp_LS_LS-test-clean
+
+python calc_abx.py --data-dir=exp_LS_LS-test-clean
+
