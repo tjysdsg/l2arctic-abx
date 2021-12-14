82795fd10a7b03ee339208af9b1cffa96190cb11
tjysdsg <tjy1018543509@163.com>
2021-12-14 21:23:01 +0800

    Fix shortcut scripts

--- test_LS-model_LS-test-clean.sh
+++ test_LS-model_LS-test-clean.sh
@@ -8,6 +8,5 @@ python calc_abx.py --data-dir=exp_LS_LS-test-clean
 
 python count_phone_code_map.py \
   --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_index_librispeech960_LS-test-clean \
-  --utt2spk=/home/storage15/tangjiyang/librispeech_ali_test_clean/kaldi_format/utt2spk \
   --phone-alignment=/home/storage15/tangjiyang/librispeech_ali_test_clean/align.txt \
   --out-dir=exp_LS_LS-test-clean
