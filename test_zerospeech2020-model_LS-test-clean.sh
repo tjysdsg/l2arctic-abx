70f2cc8cdbfd0e6bc453f74e5176da7c9cc61d11
tjysdsg <tjy1018543509@163.com>
2021-12-17 19:14:31 +0800

    FIx test_zerospeech2020-model_LS-test-clean.sh

--- test_zerospeech2020-model_LS-test-clean.sh
+++ test_zerospeech2020-model_LS-test-clean.sh
@@ -1,11 +1,12 @@
 python generate_item_file.py \
   --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_zerospeech2020_LS-test-clean \
-  --phone-alignment=phone_alignment.txt \
+  --utt2spk=/home/storage15/tangjiyang/librispeech_ali_test_clean/kaldi_format/utt2spk \
+  --phone-alignment=/home/storage15/tangjiyang/librispeech_ali_test_clean/align.txt \
   --out-dir=exp_zerospeech2020_LS-test-clean
 
 python calc_abx.py --data-dir=exp_zerospeech2020_LS-test-clean
 
 python count_phone_code_map.py \
   --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/zerospeech_vae/encode_index_zerospeech2020_LS-test-clean \
-  --phone-alignment=phone_alignment.txt \
+  --phone-alignment=/home/storage15/tangjiyang/librispeech_ali_test_clean/align.txt \
   --out-dir=exp_zerospeech2020_LS-test-clean
