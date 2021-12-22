26d194da5fd86504fefdcb46f4d75f9975d5bce7
唐际洋 <tangjiyang@xiaomi.com>
2021-12-22 11:50:52 +0800

    Add shortcut

---
+++ tsne_wav2vec2_l2arctic.sh
@@ -0,0 +1,6 @@
+python visualize_tsne.py \
+  --encode-dir=/home/storage15/tangjiyang/DAU-MD/exp/wav2vec2/encode_wav2vec2_l2arctic \
+  --phone-alignment=phone_alignment.txt \
+  --model-config=config/wav2vec2.json \
+  --sample-rate=16000 \
+  --out-dir=exp_wav2vec2_l2arctic/viz
